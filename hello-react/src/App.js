f26467b27da60d73dc7c2d114ce37b03122557c0
fatichafs <fatichahsaadah@gmail.com>
2025-03-22 10:21:02 +0700

    Pertemuan_2

---
+++ hello-react/src/App.js
@@ -0,0 +1,19 @@
+// Impoprtv Libraries
+import React from "react"; 
+import Header from "./Header.jsx"
+
+
+// Membuat komponen dengan class
+
+// Create Component
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>nama saya caca</p>
+      <Header />
+    </div>
+  );
+};
+//Export Default
+export default App;
---
+++ hello-react/src/Header.jsx
@@ -0,0 +1,20 @@
+// import libraries
+import React from "react";
+
+
+// Membuat Komponen dengan Class
+
+
+// Membuat komponen dengan class
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Makanan Khas Indonesia</h1>
+      </div>
+    );
+  }
+}
+  //Export Default
+  export default Header;
+  
